5b6826a77aeed92e217727a09c7995173ee77f22
Rick <rick.r.woods@gmail.com>
2022-12-01 19:02:56 -0800

    Db model for pprx + removing django default cruft

--- pprxweb/pprxweb/settings.py
+++ pprxweb/pprxweb/settings.py
@@ -31,40 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'scorebrowser.apps.ScorebrowserConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'pprxweb.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
 ]
 
 WSGI_APPLICATION = 'pprxweb.wsgi.application'
@@ -85,18 +64,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
 ]
 
 
--- pprxweb/scorebrowser/models.py
+++ pprxweb/scorebrowser/models.py
@@ -1,3 +1,73 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class ChartPairs(models.Model):
+    id = models.AutoField(primary_key=True)
+    x_chart_id = models.IntegerField()
+    y_chart_id = models.IntegerField()
+    slope = models.FloatField()
+    strength = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'chart_pairs'
+
+
+class Charts(models.Model):
+    id = models.AutoField(primary_key=True)
+    song_id = models.TextField()
+    difficulty = models.IntegerField()
+    rating = models.IntegerField()
+    spice = models.FloatField(blank=True, null=True)
+    raw_spice = models.FloatField(blank=True, null=True)
+    popular = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'charts'
+
+
+class Players(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'players'
+
+
+class Scores(models.Model):
+    id = models.AutoField(primary_key=True)
+    player_id = models.IntegerField()
+    chart_id = models.IntegerField()
+    score = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'scores'
+
+
+class Songs(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'songs'
+
+
+class UnprocessedPairs(models.Model):
+    id = models.AutoField(primary_key=True)
+    x_chart_id = models.IntegerField()
+    y_chart_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'unprocessed_pairs'
