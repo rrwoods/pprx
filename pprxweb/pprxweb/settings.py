9a4c8fddeec30bb3f5d7de1810ba5adf19b3e5a1
Rick <rick.r.woods@gmail.com>
2023-02-08 19:48:37 -0800

    Make app publishable
    - Route / to scorebrowser
    - allow pprx.gg domain

--- pprxweb/pprxweb/settings.py
+++ pprxweb/pprxweb/settings.py
@@ -71,15 +71,7 @@ WSGI_APPLICATION = 'pprxweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if False: # true for aws w/sqlite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'pprx.db',
-        }
-    }
-    ALLOWED_HOSTS = ['pprx-dev.us-west-2.elasticbeanstalk.com']
-elif 'RDS_HOSTNAME' in os.environ:
+if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -96,6 +88,7 @@ elif 'RDS_HOSTNAME' in os.environ:
     }
     ALLOWED_HOSTS = [
         'pprx-dev.us-west-2.elasticbeanstalk.com',
+        'pprx.gg',
     ]
 else:
     DATABASES = {
--- pprxweb/pprxweb/urls.py
+++ pprxweb/pprxweb/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/scorebrowser/')),
     path('admin/', admin.site.urls),
     path('scorebrowser/', include('scorebrowser.urls')),
 ]
