041968cffd71a229179bf18bcc3ea581e11d5ab5
Rick <rick.r.woods@gmail.com>
2023-03-17 18:31:17 -0700

    Check/uncheck all for unlocks

--- pprxweb/scorebrowser/urls.py
+++ pprxweb/scorebrowser/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	path('scores', views.scores, name='scores'),
 	path('unlocks', views.unlocks, name='unlocks'),
 	path('update_unlock', views.update_unlock, name='update_unlock'),
+	path('update_unlock_event', views.update_unlock_event, name='update_unlock_event'),
 	path('set_goal', views.set_goal, name='set_goal'),
 
 	path('hello', views.hello, name='hello'),
