aa9b40fb93182546b017c290518d433e97f02674
Rick <rick.r.woods@gmail.com>
2023-05-17 21:19:48 -0700

    more unlocks

---
+++ pprxweb/scorebrowser/migrations/0046_new_gl_survival.py
@@ -0,0 +1,45 @@
+from django.db import migrations, models
+
+
+def forward(apps, schema_editor):
+	Song = apps.get_model("scorebrowser", "Song")
+	UnlockTask = apps.get_model("scorebrowser", "UnlockTask")
+	ChartUnlock = apps.get_model("scorebrowser", "ChartUnlock")
+	Chart = apps.get_model("scorebrowser", "Chart")
+	SongLock = apps.get_model("scorebrowser", "SongLock")
+	CabinetModel = apps.get_model("scorebrowser", "CabinetModel")
+
+	def findSong(title):
+		candidates = Song.objects.filter(title=title)
+		for song in candidates:
+			if song.title == title:
+				return song
+
+	def findChart(title, difficulty_id):
+		candidates = Chart.objects.filter(song__title=title, difficulty_id=difficulty_id)
+		for chart in candidates:
+			if chart.song.title == title:
+				return chart
+		return None
+
+	def newGoldenLeague(title, difficulty_ids):
+		advanceBorder = UnlockTask.objects.filter(name='GOLD CLASS with advanced border').first()
+		for d in difficulty_ids:
+			ChartUnlock.objects.create(version_id=19, task=advanceBorder, chart=findChart(title, d))
+
+		white = CabinetModel.objects.get(name="white")
+		SongLock.objects.create(version_id=19, model=white, song=findSong(title))
+
+	newGoldenLeague('SURVIVAL AT THE END OF THE UNIVERSE', [2, 3])
+
+
+def backward(apps, schema_editor):
+	pass
+
+
+class Migration(migrations.Migration):
+	dependencies = [('scorebrowser', '0045_gl_rotate_ability')]
+
+	operations = [migrations.RunPython(forward, backward)]
+
+
