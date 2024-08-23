5c11248710d505f5126c777fb8e9943ee5fb8652
rrwoods <rick.r.woods@gmail.com>
2024-08-23 07:49:04 -0700

    Revert "Progress reporting on updatequality command"
    This reverts commit 0fd7f1adde4eb73c5a7464c4ed469b9dd2f54de1.

--- pprxweb/scorebrowser/management/commands/updatequality.py
+++ pprxweb/scorebrowser/management/commands/updatequality.py
@@ -1,14 +1,6 @@
-from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 from scorebrowser.models import *
 import math
-
-
-def report(start, done, remaining):
-	current = datetime.now()
-	each = (current - start) / done
-	when = (each * remaining) + current
-	print ("{} done, {} remaining; estimated completion at {}".format(done, remaining, when))
 
 
 class Command(BaseCommand):
@@ -16,19 +8,9 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		updated_quality = []
-		all_scores = UserScore.objects.all().select_related('chart')
-		remaining = len(all_scores)
-		done = 0
-		start = datetime.now()
-
-		for score in all_scores:
-			done += 1
-			remaining -= 1
-
+		for score in UserScore.objects.all().select_related('chart'):
 			if not score.chart.spice:
 				continue
 			score.quality = score.chart.spice - math.log2((1000001 - min(score.score, 999000))/1000000)
 			updated_quality.append(score)
-
-			report(start, done, remaining)
 		UserScore.objects.bulk_update(updated_quality, ['quality'])
