750037dc78d2199ea95d6598e91145f9f0d95d7c
rrwoods <rick.r.woods@gmail.com>
2026-01-11 18:13:56 -0800

    Diagnosing some errors

--- pprxweb/pprxweb/settings.py
+++ pprxweb/pprxweb/settings.py
@@ -98,7 +98,7 @@ if 'DATABASE_URL' in env:
         'pprx-prod-pe9zb.ondigitalocean.app',
         'pprx.gg',
     ]
-    DEBUG = False
+    DEBUG = True
 elif 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
